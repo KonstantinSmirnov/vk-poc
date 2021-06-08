ffbcab67d4292279a7da10df00f38045663335ac
konstantinsmirnov <k.smi@mail.ru>
2021-06-09 01:16:15 +0300

    update instructions;2C

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,6 +11,7 @@ class PagesController < ApplicationController
   def build_authorize_url
     url_base = "https://oauth.vk.com/authorize"
     client_id = @setting.vk_app_id
+    client_secret = @setting.vk_app_secret
     display = "page"
     redirect_uri = "https://warm-savannah-24055.herokuapp.com/"
     scope = "market"
@@ -20,7 +21,7 @@ class PagesController < ApplicationController
     full_url_request = url_base + "?client_id=" + client_id + "&display=" + display + "&redirect_uri=" + redirect_uri + "&scope=" + scope + "&response_type=" + response_type + "&v=" + api_version
 
     @authorize_url = full_url_request
-
+    @incomplete_authorization_url = "https://oauth.vk.com/access_token" + "?client_id=" + client_id + "&client_secret=" + client_secret + "&redirect_uri=" + redirect_uri + "&code="
 
   end
 
