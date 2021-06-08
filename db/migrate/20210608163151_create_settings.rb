d806695eb01d87bda3078a164404e485fd4a1e7e
konstantinsmirnov <k.smi@mail.ru>
2021-06-08 21:01:42 +0300

    Add user token field

--- db/migrate/20210608163151_create_settings.rb
+++ db/migrate/20210608163151_create_settings.rb
@@ -5,6 +5,8 @@ class CreateSettings < ActiveRecord::Migration[6.1]
       t.string :vk_app_secret
       t.string :vk_api_version
 
+      t.string :vk_access_token
+
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2021_06_08_163151) do
     t.string "vk_app_id"
     t.string "vk_app_secret"
     t.string "vk_api_version"
+    t.string "vk_access_token"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
